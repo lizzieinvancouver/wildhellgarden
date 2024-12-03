42f6ab70ec11dda8f14ededde525457d80d73d39
dbuona <dbuonaiuto@g.harvard.edu>
2024-12-03 12:31:25 -0500

    worked on figures

--- analyses/wildtimes_analysis.R
+++ analyses/wildtimes_analysis.R
@@ -91,6 +91,7 @@ pgs.mod<-brm(pgs~leafout_cent+(leafout_cent|spp),data=cg1,control = list(adapt_d
 conditional_effects(pgsGDD.mod)
 conditional_effects(pgs.mod)
 
+if(FALSE){
 ###do the same models for full growing season
 cg2$fgsGDD_cent<-cg2$fgsGDD-mean(cg2$fgsGDD)
 cg2$leafout_cent<-cg2$leafout-mean(cg2$leafout)
@@ -98,10 +99,12 @@ cg2$leafout_cent<-cg2$leafout-mean(cg2$leafout)
 fgsGDD.mod<-brm(fgsGDD~leafout_cent+(leafout_cent|spp),data=cg2,control = list(adapt_delta=.95),warmup=3000,iter=4000)
 
 fgs.mod<-brm(fgs~leafout_cent+(leafout_cent|spp),data=cg2,control = list(adapt_delta=.95),warmup=3000,iter=4000)
-
+}
 ###make posterior predictions
 
 new.data<-data.frame(leafout_cent=rep(c(-20,0,20),each=18),spp=rep(unique(cg1$spp),3)) ## generate a new data frame
+
+if(FALSE){
 FGDD<-epred_draws(fgsGDD.mod,newdata = new.data,ndraws = 1000)### prediction thermal model for each species
 Fp<-epred_draws(fgs.mod,newdata = new.data,ndraws = 1000)### prediction for calaendar each species
 FGDD2<-epred_draws(fgsGDD.mod,newdata = new.data,ndraws = 1000,re_formula = NA)### prediction thermal model for each species
@@ -127,7 +130,7 @@ b<-ggplot(F2,aes(leafout_cent,.epred))+geom_line(size=0.01,aes(group=.draw))+geo
   ggthemes::theme_few()
 
 ggpubr::ggarrange(b,a)
-
+}
 ###same thing for primary growing season
 PGDD<-epred_draws(pgsGDD.mod,newdata = new.data,ndraws = 1000)
 Pp<-epred_draws(pgs.mod,newdata = new.data,ndraws = 1000)
@@ -142,21 +145,55 @@ order<-c("SAMRAC","VIBCAS","SPIALB","AMECAN","DIELON","AROMEL","SPITOM","BETPOP"
 Pp2<-filter(Pp,spp %in% order)
 
 
+Pp2$species<-NA
+Pp2$species<-ifelse(Pp2$spp=="SAMRAC","S. racemosa",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="VIBCAS","V. cassinoides",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="SPIALB","S. alba",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="AMECAN","A. canadensis",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="DIELON","D. lonicera",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="AROMEL","A. melanocarpa",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="SPITOM","S. tomentosa",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="BETPOP","B. populifolia",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="MYRGAL","M. gale",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="BETPAP","B. papyrifera",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="BETALL","B. alleghaniensis",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="ALNINC","A. incana",Pp2$species)
+Pp2$species<-ifelse(Pp2$spp=="SORAME","S. americana",Pp2$species)
+
+
 aa<-ggplot(PGDD2,aes(leafout_cent,.epred))+geom_line(size=0.01,aes(group=.draw))+geom_smooth(method="lm")+ylab("Thermal growing season \n(primary)")+
   ggthemes::theme_few()
 
 PGGD2<-filter(PGDD,spp %in% order)
+PGGD2$species<-NA
+PGGD2$species<-ifelse(PGGD2$spp=="SAMRAC","S. racemosa",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="VIBCAS","V. cassinoides",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="SPIALB","S. alba",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="AMECAN","A. canadensis",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="DIELON","D. lonicera",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="AROMEL","A. melanocarpa",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="SPITOM","S. tomentosa",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="BETPOP","B. populifolia",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="MYRGAL","M. gale",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="BETPAP","B. papyrifera",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="BETALL","B. alleghaniensis",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="ALNINC","A. incana",PGGD2$species)
+PGGD2$species<-ifelse(PGGD2$spp=="SORAME","S. americana",PGGD2$species)
+
+
+order<-c("S. racemosa","V. cassinoides","S. alba","A. canadensis","D. lonicera","A. melanocarpa","S. tomentosa","B. populifolia","M. gale","B. papyrifera","B. alleghaniensis","A. incana","S. americana")
 
 cc<-ggplot(PGGD2,aes(leafout_cent,.epred))+geom_line(size=0.01,aes(group=grouper))+geom_smooth(method="lm")+
-  ylab("Thermal growing season \n(primary)")+
-  ggthemes::theme_few()+facet_wrap(~factor(spp, levels=order))
+  ylab("")+xlab("leafout anomaly \n(days)")+
+  ggthemes::theme_few(base_size = 11)+facet_wrap(~factor(species, levels=order)) + theme(strip.text = element_text(face = "italic"))
 
 dd<-ggplot(Pp2,aes(leafout_cent,.epred))+geom_line(size=0.01,aes(group=grouper))+geom_smooth(method="lm")+
-  ylab("Calander growing season \n(primary)")+
-  ggthemes::theme_few()+facet_wrap(~factor(spp, levels=order))
+  ylab("")+xlab("")+
+  ggthemes::theme_few(base_size = 11)+facet_wrap(~factor(species, levels=order)) + theme(strip.text = element_text(face = "italic"))
 
 ggpubr::ggarrange(cc,dd,ncol=1)
 
+if(FALSE){
 a<-ggplot()+geom_point(data=cg2,aes(x=leafout_cent,fgs),size=0.1)+
   geom_line(data=F2,size=0.01,aes(group=.draw,x=leafout_cent,y=.epred))+
   geom_smooth(data=F2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Calander growing season \n(secondary growth)")+
@@ -166,20 +203,20 @@ b<-ggplot()+geom_point(data=cg2,aes(x=leafout_cent,fgsGDD),size=0.1)+
   geom_line(data=FGDD2,size=0.01,aes(group=.draw,x=leafout_cent,y=.epred))+
   geom_smooth(data=FGDD2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Thermal growing season \n(secondary growth)")+
   ggthemes::theme_few()+coord_cartesian(xlim=c(-20,20))+xlab("SoS \n(deviation from mean)")
-
+}
 aa<-ggplot()+geom_point(data=cg1,aes(x=leafout_cent,pgs),size=0.1)+
 geom_line(data=P2,size=0.01,aes(group=.draw,x=leafout_cent,y=.epred))+
-  geom_smooth(data=P2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Calander growing season \n(primary growth)")+
-  ggthemes::theme_few()+coord_cartesian(xlim=c(-20,20))+xlab("SoS \n(deviation from mean)")
+  geom_smooth(data=P2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Calander growing season")+
+  ggthemes::theme_few()+coord_cartesian(xlim=c(-20,20))+xlab("")
 
 
 bb<-ggplot()+geom_point(data=cg1,aes(x=leafout_cent,pgsGDD),size=0.1)+
   geom_line(data=PGDD2,size=0.01,aes(group=.draw,x=leafout_cent,y=.epred))+
-  geom_smooth(data=PGDD2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Thermal growing season \n(primary growth)")+
-  ggthemes::theme_few()+coord_cartesian(xlim=c(-20,20))+xlab("SoS \n(deviation from mean)")
+  geom_smooth(data=PGDD2,aes(x=leafout_cent,y=.epred),method="lm")+ylab("Thermal growing season")+
+  ggthemes::theme_few()+coord_cartesian(xlim=c(-20,20))+xlab("SoS anomaly \n(days)")
 
 ## This is figure 3
-jpeg("figures/primarygrowingseason_modplots.jpeg", height=8,width=7,unit='in',res=200)
+jpeg("figures/primarygrowingseason_modplots.jpeg", height=8,width=9,unit='in',res=200)
 ggpubr::ggarrange(aa,dd,bb,cc,labels = c("a)","b)","c","d)"))
 dev.off()
 
@@ -216,91 +253,178 @@ mod.pgs<-brm(pgs~(1|spp)+(1|site)+(1|year),data=use.data,
 
 ##make leafout plots
 bb.sppout<-mod.lo %>% spread_draws(r_spp[spp,Intercept])
-bb.sppout<-filter(bb.sppout, spp %in% order)
+bb.sppout$species<-NA
+bb.sppout$species<-ifelse(bb.sppout$spp=="SAMRAC","S. racemosa",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="VIBCAS","V. cassinoides",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="SPIALB","S. alba",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="AMECAN","A. canadensis",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="DIELON","D. lonicera",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="AROMEL","A. melanocarpa",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="SPITOM","S. tomentosa",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="BETPOP","B. populifolia",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="MYRGAL","M. gale",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="BETPAP","B. papyrifera",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="BETALL","B. alleghaniensis",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="ALNINC","A. incana",bb.sppout$species)
+bb.sppout$species<-ifelse(bb.sppout$spp=="SORAME","S. americana",bb.sppout$species)
+bb.sppout<-filter(bb.sppout, species %in% order)
 
 bs.sppout<-mod.bs %>% spread_draws(r_spp[spp,Intercept])
-bs.sppout<-filter(bs.sppout, spp %in% order)
+bs.sppout$species<-NA
+bs.sppout$species<-ifelse(bs.sppout$spp=="SAMRAC","S. racemosa",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="VIBCAS","V. cassinoides",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="SPIALB","S. alba",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="AMECAN","A. canadensis",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="DIELON","D. lonicera",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="AROMEL","A. melanocarpa",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="SPITOM","S. tomentosa",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="BETPOP","B. populifolia",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="MYRGAL","M. gale",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="BETPAP","B. papyrifera",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="BETALL","B. alleghaniensis",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="ALNINC","A. incana",bs.sppout$species)
+bs.sppout$species<-ifelse(bs.sppout$spp=="SORAME","S. americana",bs.sppout$species)
+bs.sppout<-filter(bs.sppout, species %in% order)
 
 pgs.sppout<-mod.pgs %>% spread_draws(r_spp[spp,Intercept])
-pgs.sppout<-filter(pgs.sppout, spp %in% order)
+pgs.sppout$species<-NA
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="SAMRAC","S. racemosa",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="VIBCAS","V. cassinoides",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="SPIALB","S. alba",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="AMECAN","A. canadensis",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="DIELON","D. lonicera",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="AROMEL","A. melanocarpa",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="SPITOM","S. tomentosa",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="BETPOP","B. populifolia",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="MYRGAL","M. gale",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="BETPAP","B. papyrifera",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="BETALL","B. alleghaniensis",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="ALNINC","A. incana",pgs.sppout$species)
+pgs.sppout$species<-ifelse(pgs.sppout$spp=="SORAME","S. americana",pgs.sppout$species)
+pgs.sppout<-filter(pgs.sppout, species %in% order)
 
-loplot<-ggplot(bb.sppout,aes(r_spp,reorder(spp,r_spp)))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+xlim(-15,15)+
+loplot<-ggplot(bb.sppout,aes(r_spp,reorder(species,r_spp)))+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+xlim(-15,15)+
   xlab("leafout")+
-  ylab("")+
-  ggthemes::theme_few()
+  ylab("")+ggthemes::theme_few()+theme(axis.text.y = element_text(face = "italic"))
 
-bsplot<-ggplot(bs.sppout,aes(r_spp,spp))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+bsplot<-ggplot(bs.sppout,aes(r_spp,species))+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlim(-30,20)+xlab("budset")+
-  ggthemes::theme_few()+scale_y_discrete(name="",limits=order)
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=order)+theme(axis.text.y = element_blank(),
+                                                                     axis.ticks.y = element_blank())
 
-plotdur<-ggplot(pgs.sppout,aes(r_spp,spp))+
+plotdur<-ggplot(pgs.sppout,aes(r_spp,species))+
   stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("growing season")+
-  ggthemes::theme_few()+scale_y_discrete(name="",limits=order)
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=order)+theme(axis.text.y = element_blank(),
+                                                                     axis.ticks.y = element_blank())
 
-spcis<-ggpubr::ggarrange(loplot,bsplot,plotdur,ncol=3)
+spcis<-ggpubr::ggarrange(loplot,bsplot,plotdur,ncol=3,widths = c(3,2,2))
 
 ###populate effects
-bb.sppout<-mod.lo %>% spread_draws(r_spp[spp,Intercept])
-
-bb.sppout<-filter(bb.sppout, spp %in% order)
+frostfree<-c("Harvard Forest, MA, USA","Saint Hippolyte, QC, CA","White Mountains, NH, USA","Second College Grant, NH, USA")
 
 lo.siteout<-mod.lo %>% spread_draws(r_site[site,Intercept])
-bs.siteout<-mod.bs %>% spread_draws(r_site[site,Intercept])
-pgs.siteout<-mod.pgs %>% spread_draws(r_site[site,Intercept])
+lo.siteout$population<-NA
+lo.siteout$population<-ifelse(lo.siteout$site=="WM","White Mountains, NH, USA",lo.siteout$population)
+lo.siteout$population<-ifelse(lo.siteout$site=="HF","Harvard Forest, MA, USA",lo.siteout$population)
+lo.siteout$population<-ifelse(lo.siteout$site=="GR","Second College Grant, NH, USA",lo.siteout$population)
+lo.siteout$population<-ifelse(lo.siteout$site=="SH","Saint Hippolyte, QC, CA",lo.siteout$population)
 
-losite<-ggplot(lo.siteout,aes(r_site,site))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+
+bs.siteout<-mod.bs %>% spread_draws(r_site[site,Intercept])
+
+bs.siteout$population<-NA
+bs.siteout$population<-ifelse(bs.siteout$site=="WM","White Mountains, NH, USA",bs.siteout$population)
+bs.siteout$population<-ifelse(bs.siteout$site=="HF","Harvard Forest, MA, USA",bs.siteout$population)
+bs.siteout$population<-ifelse(bs.siteout$site=="GR","Second College Grant, NH, USA",bs.siteout$population)
+bs.siteout$population<-ifelse(bs.siteout$site=="SH","Saint Hippolyte, QC, CA",bs.siteout$population)
+
+
+
+
+pgs.siteout<-mod.pgs %>% spread_draws(r_site[site,Intercept])
+pgs.siteout$population<-NA
+pgs.siteout$population<-ifelse(pgs.siteout$site=="WM","White Mountains, NH, USA",pgs.siteout$population)
+pgs.siteout$population<-ifelse(pgs.siteout$site=="HF","Harvard Forest, MA, USA",pgs.siteout$population)
+pgs.siteout$population<-ifelse(pgs.siteout$site=="GR","Second College Grant, NH, USA",pgs.siteout$population)
+pgs.siteout$population<-ifelse(pgs.siteout$site=="SH","Saint Hippolyte, QC, CA",pgs.siteout$population)
+
+
+
+
+losite<-ggplot(lo.siteout,aes(r_site,population))+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("leafout")+
   ylab("")+
-  ggthemes::theme_few()
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(frostfree))
 
-bssite<-ggplot(bs.siteout,aes(r_site,site))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+bssite<-ggplot(bs.siteout,aes(r_site,population))+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("budset")+
   ylab("")+
-  ggthemes::theme_few()
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(frostfree))+theme(axis.text.y = element_blank(),
+                                                                              axis.ticks.y = element_blank())
 
-pgssite<-ggplot(pgs.siteout,aes(r_site,site))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+pgssite<-ggplot(pgs.siteout,aes(r_site,population))+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("growing season")+
   ylab("")+
-  ggthemes::theme_few()
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(frostfree))+theme(axis.text.y = element_blank(),
+                                                                              axis.ticks.y = element_blank())
 
-sties<-ggpubr::ggarrange(losite,bssite,pgssite,ncol=3)
+sties<-ggpubr::ggarrange(losite,bssite,pgssite,ncol=3,widths = c(4,2,2))
 
 lo.yearout<-mod.lo %>% spread_draws(r_year[year,Intercept])
 bs.yearout<-mod.bs %>% spread_draws(r_year[year,Intercept])
 pgs.yearout<-mod.pgs %>% spread_draws(r_year[year,Intercept])
 
+yorder<-c("2018","2019","2020")
 loyear<-ggplot(lo.yearout,aes(r_year,as.factor(year)))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("leafout")+
-  ylab("")+
-  ggthemes::theme_few()
+  ylab("")+xlim(-40,40)+
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(yorder))
 
 bsyear<-ggplot(bs.yearout,aes(r_year,as.factor(year)))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("budset")+
-  ylab("")+
-  ggthemes::theme_few()
+  ylab("")+xlim(-40,40)+
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(yorder))+theme(axis.text.y = element_blank(),
+                                                                           axis.ticks.y = element_blank())
+
+
 
 pgsyear<-ggplot(pgs.yearout,aes(r_year,as.factor(year)))+
-  stat_halfeye(.width = c(.5),alpha=0.6)+geom_vline(xintercept=0)+
+  stat_halfeye(.width = c(.5,.9),alpha=0.6)+geom_vline(xintercept=0)+
   xlab("growing season")+
-  ylab("")+
-  ggthemes::theme_few()
+  ylab("")+xlim(-40,40)+
+  ggthemes::theme_few()+scale_y_discrete(name="",limits=rev(yorder))+theme(axis.text.y = element_blank(),
+                                                                           axis.ticks.y = element_blank())
+
+
 
 yrs<-ggpubr::ggarrange(loyear,bsyear,pgsyear,ncol=3)
 
 ## current figure 2
 jpeg("figures/var_parts.jpeg", height=8,width=7,unit='in',res=200)
-ggpubr::ggarrange(spcis,sties,yrs,ncol=1,labels=c("a)","b)","c)"),heights=c(1.6,1.2,1))
+ggpubr::ggarrange(spcis,sties,yrs,ncol=1,labels=c("a)","b)","c)"),heights=c(2,1.2,1))
 dev.off()
 
 save.image("cgseasonmods.Rda")
+
+
+new.daterz<-data.frame(spp=rep(unique(bb.sppout$spp),each=4),site=rep(unique(lo.siteout$site),13))
+lopred<- mod.lo %>% 
+  epred_draws(newdata =new.daterz,ndraws = 1000,re_formula = ~(1|spp)+(1|site))
+library(tidybayes)
+pd<-position_dodge(.5)
+ggplot(lopred,aes(spp,.epred))+stat_pointinterval(aes(color=site),.width = c(.5),position=pd)
+
+jpeg("figures/SPxPOP.jpeg")
+ggplot(lopred,aes(spp,.epred))+stat_pointinterval(aes(color=site),.width = c(.5),position=pd)
+dev.off()
 
 ### quesiton, does indiviudal variation cahnge things?
 use.data$indy<-paste(use.data$spp,use.data$site,use.data$ind,use.data$plot)### give and unique identifier
